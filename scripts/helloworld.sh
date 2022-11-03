9ef6f300c87f42504905e81f1d2b93e4988db429
gfw-breaker <lixiaoming0513@gmail.com>
2022-11-03 08:32:30 +0800

    同步于: 11月3日8时32分

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -2,7 +2,8 @@
 
 #newip=95.179.192.143
 #newip=129.146.189.110 
-newip=209.250.226.216
+#newip=209.250.226.216
+newip=129.146.143.75
 
 ip=$(cat macros.py | grep "site_base_url =" | awk -F'/' '{print $3}' | cut -d':' -f1)
 
